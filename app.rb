97d78f7017f51f2584b91ae449c43cdb57b1c8f0
Melissa Vogelgesangcat ~/.ssh/id_rsa.pubssh-keygenssh-rsassh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/id_rsa.pub <mvogelgesang01@gmail.com>
2021-12-06 16:27:10 -0600

    commit tree 48fa81596bd41d566d759e36e2396035eca23045

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    get '/' do
+        "Hello, World!"
+      end
+
+      get '/name' do
+        "My name is #{params['name']}!"
+      end
+
+      get '/hometown' do
+        "My hometown is #{params['hometown']}!"
+      end
+
+      get '/favoritesong'
+      "My favorite son is #{params['favroitesong']}!"
+    end
+
+
